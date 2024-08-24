4171677a2432d017c7d58a0248a4fe49a488b16c
Renato Delboni <87453991+Netsujr@users.noreply.github.com>
2024-08-24 15:53:58 +0900

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       backgroundImage: {
         "custom-bg":
           "url('/assets/images/ant-rozetsky-HXOllTSwrpM-unsplash@2x.png')",
+      },
+      fontFamily: {
+        inter: ["InterVariable", "sans-serif"],
       },
     },
   },
